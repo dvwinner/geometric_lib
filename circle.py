7d1cedece6fb6a587eaf499a58060c0ec6c63fb4
dvwinner <dvwinner.0408@gmail.com>
2024-10-10 00:12:15 +0300

    Added comments in circle.py funcs

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимает радиус круга -> Возвращает его площадь'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает радиус круга -> Возвращает его периметр'''
     return 2 * math.pi * r
 
